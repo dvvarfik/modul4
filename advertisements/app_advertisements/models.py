e21dae258a9e21031ee7266fa4ef969b3367d7e2
dvvarfik <sham_milena@mail.ru>
2023-09-29 23:30:35 +0300

    --

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from  django.utils.html import format_html
+
 class Advertisements(models.Model):
     title = models.CharField("Заголовок", max_length=128)
     description = models.TextField("Описание")
@@ -7,6 +10,15 @@ class Advertisements(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)\
 
+    @admin.display(description="Дата создания")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
 
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
