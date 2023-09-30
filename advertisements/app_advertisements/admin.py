119501b64e6f95a4f2160d0be68c2cbe4f576386
dvvarfik <sham_milena@mail.ru>
2023-09-30 12:36:09 +0300

    5.1

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Advertisements
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 class AdvertisementAdmin(admin.ModelAdmin):
     list_display = ["id", "title", "description", "price", "auction", "created_date", "updated_date", "image"]
@@ -14,6 +16,14 @@ class AdvertisementAdmin(admin.ModelAdmin):
             'classes': ['collapse']
         })
     )
+
+    @admin.display(description='Thumbnail')
+    def thumbnail(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" height="50" width="50" />', obj.image.url)
+        return format_html('<img src="{}" height="50" width="50" />', '/static/img/adv.png')
+
+    list_display = ['title', 'price', 'thumbnail']
 
     @admin.action(description = "Убрать возможность торга")
     def make_auction_as_false(self, request, queryset):
